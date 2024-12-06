8bd6d4183e6e84552eb652c2f87dfda3e62a83f6
james-hansel <hanseljames10@gmail.com>
2024-12-06 16:00:53 +0530

    update
    updating from master branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import HomePage from './components/HomePage'
-
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Courses from "./pages/Courses";
+import Lessons from "./pages/Lessons";
+import Home from "./pages/Home";
+import Practice from "./pages/Practice";
+import Roadmap from "./pages/Roadmap";
 function App() {
+ 
+
   return (
     <>
-    <div className='bg-[#202225] text-white min-h-screen flex items-center'>
-      <HomePage></HomePage>
-    </div>
-
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/lessons" element={<Lessons />} />
+        <Route path="/practice" element={<Practice />} />
+        <Route path="/roadmap" element={<Roadmap />} />
+      </Routes>
+      </Router>
     </>
   )
 }
