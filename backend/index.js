0705dd31f33131883fbc6d955ed948771b4ff21c
james-hansel <hanseljames10@gmail.com>
2025-01-02 16:48:17 +0530

    resolved conflicts

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import express from "express";
-const app = express();
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-
-import userRoutes from "./routes/user.routes.js"
-import problemRoutes from "./routes/problem.routes.js"
-
-app.use(express.json());
-app.use(cors());
-
-app.use("/user", userRoutes);
-app.use("/problems", problemRoutes);
-
-dotenv.config();
-
-mongoose.connect(process.env.URI).then(()=> {
-  
-  console.log("Connected successfully");
-
-  app.listen(process.env.PORT || 8000, (error) => {
-    if(error) console.log(err);
-    console.log("Running Successfully at : " + process.env.PORT)
-  });
-  
-}).catch((error) => {
-  
-  console.log("Error occurred : ", error);
-  
-=======
 import express from 'express';
 import bodyParser from 'body-parser';
 import { exec } from 'child_process';
@@ -39,6 +7,8 @@ import cors from 'cors'; // Import CORS package
 import { fileURLToPath } from 'url'; // For converting URL to file path
 import { dirname } from 'path'; // For getting directory name from a file path
 import testCaseRoutes from './routes/testcases.js'
+import userRoutes from "./routes/user.routes.js"
+import problemRoutes from "./routes/problem.routes.js"
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 
@@ -62,8 +32,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/testcases', testCaseRoutes)
-
-
+app.use("/user", userRoutes);
+app.use("/problems", problemRoutes);
 
 // Define the POST route for running code
 app.post('/run', async (req, res) => {
@@ -311,5 +281,4 @@ mongoose.connect(process.env.URI).then(()=> {
   });
 }).catch((error) => {
   console.log("Error occurred : ", error);
->>>>>>> code-runner/master
 })
